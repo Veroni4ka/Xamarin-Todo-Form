da69908665bd4c10ba4f4647046db4ef9facc79a
Veroni4ka <veronika.kolesnikova@yahoo.com>
2017-05-11 19:29:29 -0400

    Initial commit.

---
+++ BasicApp123/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BasicApp123
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://basicapp123.azurewebsites.net";
+	}
+}
+
